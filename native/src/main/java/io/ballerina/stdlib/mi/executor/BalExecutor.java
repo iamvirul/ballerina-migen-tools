4aa001f88a26c34eb643f67dcd934bc0a4fb8926
Virul Nirmala Wickramasinge <virulwickramasinghe@gmail.com>
2026-04-26 17:02:57 +0530

    Backport runtime parameter handling fixes to JDK17 Ballerina 10 imports

--- native/src/main/java/io/ballerina/stdlib/mi/executor/BalExecutor.java
+++ native/src/main/java/io/ballerina/stdlib/mi/executor/BalExecutor.java
@@ -137,7 +137,7 @@ public class BalExecutor {
             }
         }
         Object[] args = new Object[size];
-        paramHandler.setParameters(args, context);
+        paramHandler.setParameters(args, context, callable);
 
         try {
             Object result;
@@ -161,9 +161,7 @@ public class BalExecutor {
                     result = invokeMethodSync(rt, bObject, jvmMethodName, argsWithPathParams);
                 } else {
                     String functionName = SynapseUtils.getPropertyAsString(context, Constants.FUNCTION_NAME);
-                    // Convert arguments to expected types using method signature info
-                    Object[] convertedArgs = convertArgsToExpectedTypes(bObject, functionName, args);
-                    result = invokeMethodSync(rt, bObject, functionName, convertedArgs);
+                    result = invokeMethodSync(rt, bObject, functionName, args);
                 }
             } else {
                 throw new SynapseException("Unsupported callable type: " + callable.getClass().getName());
@@ -225,17 +223,5 @@ public class BalExecutor {
 
     private static boolean isOverwriteBody(MessageContext context) {
         return Boolean.parseBoolean((String) SynapseUtils.lookupTemplateParameter(context, Constants.OVERWRITE_BODY));
-    }
-
-    /**
-     * Converts arguments to their expected types based on method signature.
-     * This is used to convert generic BMaps to typed records for external module types.
-     */
-    private Object[] convertArgsToExpectedTypes(BObject bObject, String methodName, Object[] args) {
-        Object[] converted = new Object[args.length];
-        for (int i = 0; i < args.length; i++) {
-            converted[i] = DataTransformer.convertToExpectedType(args[i], bObject, methodName, i);
-        }
-        return converted;
     }
 }
